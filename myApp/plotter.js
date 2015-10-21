f5e09f4e54052c4fd9b15db560fa7e55d20a92e0
Robin Malhotra <robinmalhotra@Rahuls-MacBook-Pro-2.local>
2015-10-21 23:55:00 +0530

    Thanks Surana and Obama

--- myApp/plotter.js
+++ myApp/plotter.js
@@ -33,13 +33,13 @@ function init() {
 	controls.minDistance = 20;
 	controls.maxDistance = 500;
 
-	scene.add( new THREE.AmbientLight( 0xffffff) );
+	scene.add( new THREE.AmbientLight( 0xeee) );
 
 	var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
 	scene.add( light );
 
 
-	var a=22,ar=5
+	var a=22,ar=11
 	var b = a/ar
 	var spi = 1/(2*(a+a/ar))
 	var fpi = 1/(4*a)
@@ -86,49 +86,162 @@ function init() {
 
 
 	// binders
-	for(var i=0 ; i<noOfRepeats-1 ; i++)
-
+	// for(var i=0 ; i<noOfRepeats-1 ; i++)
+for(var i=0 ; i<noOfRepeats-1; i++)
 	{
 		var binderPoints = []
 
-		for(var j=0; j<=(noOfRepeats-1)/2 ; j++)
+
+		// for(var j=0; j<=(noOfRepeats-1)/2 ; j++)
+		for(var j=0; j<1 ; j++)
 		{
-			console.log(j)
+
 			var dist = 4*b
+
 			if(i%2 == 0)
 			{
+				var curve = new THREE.EllipseCurve( 0, 0, // ax, aY
+								a+b, 2*b, // xRadius, yRadius
+							 Math.PI,0,// aStartAngle, aEndAngle
+								false, // aClockwise
+								0 // aRotation
+								)
+				var pts=[]
+				pts=pts.concat(curve.getPoints(10))
+				//2D to 3D conversion
+				var points3d=[]
+				for(var k=0;k<11;k++)
+				{
+					points3d = points3d.concat([new THREE.Vector3(pts[k].x,pts[k].y,2*a + 1/(bpi)*i)])
+				}
+				points3d = points3d.concat([new THREE.Vector3(a+b + 2/spi*j, 2*b ,2*a + 1/(bpi)*i),
+					new THREE.Vector3(a+b + 2/spi*j, (noOfLayers-1)*(4*b) - 2*b ,2*a + 1/(bpi)*i)])
 
-				binderPoints = binderPoints.concat( new THREE.QuadraticBezierCurve3(
-					new THREE.Vector3(-(a+b) + 2/spi*j, 0, 2*a + 1/(bpi)*i),
-					new THREE.Vector3(0 + 2/spi*j, -(dist),2*a + 1/(bpi)*i),
-					new THREE.Vector3((a+b) + 2/spi*j ,0,2*a + 1/(bpi)*i)
-				).getPoints(20).slice(1,18))
 
-				binderPoints = binderPoints.concat([ new THREE.Vector3(a+b + 2/spi*j,b/4, 2*a + 1/(bpi)*i), new THREE.Vector3(a+b + 2/spi*j, (noOfLayers-1)*(4*b) - b/4 ,2*a + 1/(bpi)*i)])
+				var curve2 = new THREE.EllipseCurve( (2*a+2*b)*(1), (noOfLayers-1)*(4*b), // ax, aY
+								a+b, 2*b, // xRadius, yRadius
+							 Math.PI,0,// aStartAngle, aEndAngle
+								true, // aClockwise
+								0 // aRotation
+								)
+				var pts2=[]
+				pts2=pts2.concat(curve2.getPoints(10))
 
-				binderPoints = binderPoints.concat(new THREE.QuadraticBezierCurve3(
-					new THREE.Vector3(a+b + 2/spi*j, (noOfLayers-1)*(4*b) ,2*a + 1/(bpi)*i),
-					new THREE.Vector3(2*(a+b) + 2/spi*j,(noOfLayers-1)*(4*b)+dist,2*a + 1/(bpi)*i) ,
-					new THREE.Vector3(3*(a+b) + 2/spi*j ,(noOfLayers-1)*(4*b)  ,2*a + 1/(bpi)*i)
-				).getPoints(20).slice(1,18))
+				for(var k=0;k<11;k++)
+				{
+					points3d = points3d.concat([new THREE.Vector3(pts2[k].x,pts2[k].y,2*a + 1/(bpi)*i)])
+				}
 
-				binderPoints = binderPoints.concat([ new THREE.Vector3(3*(a+b) + 2/spi*j, (noOfLayers-1)*(4*b) - b/4 ,2*a + 1/(bpi)*i),new THREE.Vector3(3*(a+b) + 2/spi*j,b/4, 2*a + 1/(bpi)*i)])
+				points3d = points3d.concat([ new THREE.Vector3(3*(a+b) + 2/spi*j, (noOfLayers-1)*(4*b) - 2*b ,2*a + 1/(bpi)*i),
+					new THREE.Vector3(3*(a+b) + 2/spi*j,2*b, 2*a + 1/(bpi)*i)])
 
-				binderPoints = binderPoints.concat(new THREE.QuadraticBezierCurve3(
-					new THREE.Vector3(3*(a+b) + 2/spi*j, 0, 2*a + 1/(bpi)*i),
-					new THREE.Vector3(4*(a+b) + 2/spi*j, -(dist),2*a + 1/(bpi)*i),
-					new THREE.Vector3(5*(a+b) + 2/spi*j ,0,2*a + 1/(bpi)*i)
-				).getPoints(20).slice(1,18))
+				var curve3 = new THREE.EllipseCurve( 2*(a+b)*(2), 0, // ax, aY
+								a+b, 2*b, // xRadius, yRadius
+							 	Math.PI,0,// aStartAngle, aEndAngle
+								false, // aClockwise
+								0 // aRotation
+								)
+								var pts3=[]
+								pts3=pts3.concat(curve3.getPoints(10))
+
+				for(var k=0;k<11;k++){
+				points3d = points3d.concat([new THREE.Vector3(pts3[k].x,pts3[k].y,2*a + 1/(bpi)*i)])
+				}
+				points3d = points3d.concat([ new THREE.Vector3(5*(a+b) + 2/spi*j, 2*b ,2*a + 1/(bpi)*i),
+					new THREE.Vector3(5*(a+b) + 2/spi*j,(noOfLayers-1)*(4*b)-2*b, 2*a + 1/(bpi)*i)])
+
+					var curve4 = new THREE.EllipseCurve( (2*a+2*b)*(3), (noOfLayers-1)*(4*b), // ax, aY
+									a+b, 2*b, // xRadius, yRadius
+								 Math.PI,0,// aStartAngle, aEndAngle
+									true, // aClockwise
+									0 // aRotation
+									)
+					var pts4=[]
+					pts4=pts4.concat(curve4.getPoints(10))
+
+					for(var k=0;k<11;k++)
+					{
+						points3d = points3d.concat([new THREE.Vector3(pts4[k].x,pts4[k].y,2*a + 1/(bpi)*i)])
+					}
+
+binderPoints = binderPoints.concat(points3d)
+
 			}
 			else
 			{
+				var curve = new THREE.EllipseCurve( 0, (noOfLayers-1)*4*b, // ax, aY
+								a+b, 2*b, // xRadius, yRadius
+							 Math.PI,0,// aStartAngle, aEndAngle
+								true, // aClockwise
+								0 // aRotation
+								)
+				var pts=[]
+				pts=pts.concat(curve.getPoints(10))
+				//2D to 3D conversion
+				var points3d=[]
+				for(var k=0;k<11;k++)
+				{
+					points3d = points3d.concat([new THREE.Vector3(pts[k].x,pts[k].y,2*a + 1/(bpi)*i)])
+				}
+
+				points3d = points3d.concat([new THREE.Vector3(a+b + 2/spi*j, (noOfLayers-1)*(4*b) - 2*b ,2*a + 1/(bpi)*i),new THREE.Vector3(a+b + 2/spi*j, 2*b ,2*a + 1/(bpi)*i)])
+
+
+				var curve2 = new THREE.EllipseCurve( (2*a+2*b)*(1), 0, // ax, aY
+								a+b, 2*b, // xRadius, yRadius
+							 Math.PI,0,// aStartAngle, aEndAngle
+								false, // aClockwise
+								0 // aRotation
+								)
+				var pts2=[]
+				pts2=pts2.concat(curve2.getPoints(10))
+
+				for(var k=0;k<11;k++)
+				{
+					points3d = points3d.concat([new THREE.Vector3(pts2[k].x,pts2[k].y,2*a + 1/(bpi)*i)])
+				}
+
+				points3d = points3d.concat([
+					new THREE.Vector3(3*(a+b) + 2/spi*j,2*b, 2*a + 1/(bpi)*i)],new THREE.Vector3(3*(a+b) + 2/spi*j, (noOfLayers-1)*(4*b) - 2*b ,2*a + 1/(bpi)*i))
+
+				var curve3 = new THREE.EllipseCurve( 2*(a+b)*(2), (noOfLayers-1)*4*b, // ax, aY
+								a+b, 2*b, // xRadius, yRadius
+								Math.PI,0,// aStartAngle, aEndAngle
+								true, // aClockwise
+								0 // aRotation
+								)
+								var pts3=[]
+								pts3=pts3.concat(curve3.getPoints(10))
+
+				for(var k=0;k<11;k++){
+				points3d = points3d.concat([new THREE.Vector3(pts3[k].x,pts3[k].y,2*a + 1/(bpi)*i)])
+				}
+				points3d = points3d.concat([
+					new THREE.Vector3(5*(a+b) + 2/spi*j,(noOfLayers-1)*(4*b)-2*b, 2*a + 1/(bpi)*i),new THREE.Vector3(5*(a+b) + 2/spi*j, 2*b ,2*a + 1/(bpi)*i)])
+
+					var curve4 = new THREE.EllipseCurve( (2*a+2*b)*(3), 0, // ax, aY
+									a+b, 2*b, // xRadius, yRadius
+								 Math.PI,0,// aStartAngle, aEndAngle
+									false, // aClockwise
+									0 // aRotation
+									)
+					var pts4=[]
+					pts4=pts4.concat(curve4.getPoints(10))
+
+					for(var k=0;k<11;k++)
+					{
+						points3d = points3d.concat([new THREE.Vector3(pts4[k].x,pts4[k].y,2*a + 1/(bpi)*i)])
+					}
+binderPoints = binderPoints.concat(points3d)
+/*
 				binderPoints = binderPoints.concat( new THREE.QuadraticBezierCurve3(
 					new THREE.Vector3(-(a+b) + 2/spi*j, (noOfLayers-1)*(4*b) , 2*a + 1/(bpi)*i),
 					new THREE.Vector3(0 + 2/spi*j, (noOfLayers-1)*(4*b) + dist,2*a + 1/(bpi)*i),
 					new THREE.Vector3((a+b) + 2/spi*j ,(noOfLayers-1)*(4*b) ,2*a + 1/(bpi)*i)
 				).getPoints(20).slice(1,18))
 
-				binderPoints = binderPoints.concat([ new THREE.Vector3(a+b + 2/spi*j, (noOfLayers-1)*(4*b) - b/4,2*a + 1/(bpi)*i),new THREE.Vector3(a+b + 2/spi*j,b/4, 2*a + 1/(bpi)*i)])
+				binderPoints = binderPoints.concat([ new THREE.Vector3(a+b + 2/spi*j, (noOfLayers-1)*(4*b) - b/4,2*a + 1/(bpi)*i),
+					new THREE.Vector3(a+b + 2/spi*j,b/4, 2*a + 1/(bpi)*i)])
 
 
 
@@ -138,7 +251,8 @@ function init() {
 					new THREE.Vector3(3*(a+b) + 2/spi*j ,0,2*a + 1/(bpi)*i)
 				).getPoints(20).slice(1,18))
 
-				binderPoints = binderPoints.concat([new THREE.Vector3(3*(a+b) + 2/spi*j,-b/4, 2*a + 1/(bpi)*i), new THREE.Vector3(3*(a+b) + 2/spi*j, (noOfLayers-1)*(4*b) -b/4,2*a + 1/(bpi)*i)])
+				binderPoints = binderPoints.concat([new THREE.Vector3(3*(a+b) + 2/spi*j,-b/4, 2*a + 1/(bpi)*i),
+					new THREE.Vector3(3*(a+b) + 2/spi*j, (noOfLayers-1)*(4*b) -b/4,2*a + 1/(bpi)*i)])
 
 
 				binderPoints = binderPoints.concat(new THREE.QuadraticBezierCurve3(
@@ -146,9 +260,9 @@ function init() {
 					new THREE.Vector3(4*(a+b) + 2/spi*j,(noOfLayers-1)*(4*b)+ dist,2*a + 1/(bpi)*i) ,
 					new THREE.Vector3(5*(a+b) + 2/spi*j ,(noOfLayers-1)*(4*b)  ,2*a + 1/(bpi)*i)
 				).getPoints(20).slice(1,18))
+				*/
 			}
 		}
-		console.log(binderPoints);
 		var closedSpline = new THREE.SplineCurve3(binderPoints);
 
 		var extrudeSettings = {
@@ -172,6 +286,32 @@ function init() {
 		var mesh = new THREE.Mesh( geometry, material );
 
 		scene.add( mesh );
+		// First we want to clone our original geometry.
+// Just in case we want to get the low poly version back.
+		// var smooth = geometry.clone()
+		//
+		// // Next, we need to merge vertices to clean up any unwanted vertex.
+		// smooth.mergeVertices();
+		//
+		// var subdiv = 2;
+    // var modifier = new THREE.SubdivisionModifier( subdiv );
+		//
+		// // Apply the modifier to our cloned geometry.
+		// modifier.modify( smooth );
+		//
+		// // Finally, add our new detailed geometry to a mesh object and add it to our scene.
+		// var mesh = new THREE.Mesh( smooth, new THREE.MeshLambertMaterial( { color: 0x00ff00 } ) );
+		// scene.add( mesh );
+		//
+		//
+		// var geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings);
+		//
+		// var material = new THREE.MeshLambertMaterial( { color: 0x00ff00, wireframe: false } );
+		//
+		// var mesh = new THREE.Mesh( geometry, material );
+		//
+		// scene.add( mesh );
+
 
 	}
 
